834787d3919d90ac5b4dd666b6df9d8149f88870
Michael Mygas <michalis.mgs@gmail.com>
2019-08-05 11:55:59 +0300

    dt fix

--- FilterData.go
+++ FilterData.go
@@ -1,18 +1,23 @@
 package main
 
 //get each driver
-func filterData(u userData, c chan userData) {
+func filterData(ride userData, c chan userData) {
 	var filteredArr []gpsData
-	d := u.data
+	d := ride.data
 	prevRow := gpsData{lat: d[0].lat, lng: d[0].lng, timestamp: d[0].timestamp}
 	indexStart := 1
+	var u float64
 	//check p1 with p3 and p4 -if p1 is invalid dont loose all data
 	if len(d) >= 5 {
 		initRowCheck := false
 		for i := 2; i < 5; i++ {
 			dt := d[i].timestamp - prevRow.timestamp
-			dist := harvesineFormula(prevRow.lat, prevRow.lng, d[i].lat, d[i].lng)
-			u := dist / float64(dt) * 3600
+			if dt != 0 {
+				dist := harvesineFormula(prevRow.lat, prevRow.lng, d[i].lat, d[i].lng)
+				u = dist / float64(dt) * 3600
+			} else {
+				u = 101
+			}
 			if u <= 100 {
 				initRowCheck = true
 			}
@@ -29,7 +34,11 @@ func filterData(u userData, c chan userData) {
 		dt := d[i].timestamp - prevRow.timestamp
 		dist := harvesineFormula(prevRow.lat, prevRow.lng, d[i].lat, d[i].lng)
 		//calc speed
-		u := dist / float64(dt) * 3600
+		if dt != 0 {
+			u = dist / float64(dt) * 3600
+		} else {
+			u = 101
+		}
 		//add valid data to filteredArr
 		if u <= 100 {
 			filteredArr = append(filteredArr, d[i])
@@ -37,7 +46,7 @@ func filterData(u userData, c chan userData) {
 		}
 	}
 
-	u.data = append([]gpsData(nil), filteredArr...)
+	ride.data = append([]gpsData(nil), filteredArr...)
 	//return driver to the channel
-	c <- u
+	c <- ride
 }
